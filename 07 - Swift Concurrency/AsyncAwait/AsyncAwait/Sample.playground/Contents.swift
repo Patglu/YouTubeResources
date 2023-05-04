8b7d39eca9edde6e18e721a763ae8078d464b036
Pat <113903905+Patglu@users.noreply.github.com>
2023-05-04 19:02:43 +0200

    Changes
    We have deleted the old code and started creating the async code

--- 07 - Swift Concurrency/AsyncAwait/AsyncAwait/Sample.playground/Contents.swift
+++ 07 - Swift Concurrency/AsyncAwait/AsyncAwait/Sample.playground/Contents.swift
@@ -20,48 +20,18 @@ enum TodoError: Error {
 // MARK: - Service
 
 struct TodoService {
-    
-    func fetch(completion: @escaping (Result<[Todo], Error>) -> Void) {
-        
+    /* were marking to the compiler that we want a async function,
+        The code will then be done on the background thread.
+        Using `await` creates a suspension point which suspends the thread.
+     */
+    func fetch() async throws -> [Todo]{
         let url = URL(string: "https://jsonplaceholder.typicode.com/todos")!
-        URLSession
-            .shared
-            .dataTask(with: url) { data, response, error in
-            
-                DispatchQueue.main.async {
-                    
-                    if let error = error {
-                        completion(.failure(TodoError.custom(error: error)))
-                        return
-                    }
-                    
-                    if let response = response as? HTTPURLResponse,
-                       response.statusCode == 200 {
-                        
-                        guard let data = data,
-                           let decodedData = try? JSONDecoder().decode([Todo].self, from: data) else {
-                               completion(.failure(TodoError.failedToDecode))
-                               return
-                        }
-                        
-                        completion(.success(decodedData))
-                        
-                    } else {
-                        completion(.failure(TodoError.invalidRequest))
-                    }
-                }
-
-                
-        }.resume()
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as HTTPURLResponse,
+              
+        
+        return[]
     }
 }
-
  let service = TodoService()
-service.fetch { res in
-    switch res {
-    case .success(let data):
-        print(data)
-    case .failure(let error):
-        print(error)
-    }
-}
